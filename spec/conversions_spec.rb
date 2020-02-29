7fb2adecf9b32a527310b688c526c79d39738bdf
Xiaoyi Liu <lxy878@gmail.com>
2020-02-29 21:08:16 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../conversions.rb'
 
-describe "conversions" do
+describe "../conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
       grams = ounces_to_grams(0)
